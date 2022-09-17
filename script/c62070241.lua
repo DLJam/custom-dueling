ef78a29ff70e2b179fd211b288fac5a3216c996d
DLJam <alexjor72@gmail.com>
2022-09-17 17:41:01 -0400

    Testin' some stuff

---
+++ script/c62070241.lua
@@ -0,0 +1,28 @@
+--Snow Shard Storm
+local s,id=GetID()
+function c62070241.initial_effect(c)
+	--Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)	
+end
+
+s.listed_series={0x85a}
+
+function s.thfilter(c)
+	return c:IsSetCard(0x85a) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectEffectYesNo(tp,e:GetHandler(),95) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
