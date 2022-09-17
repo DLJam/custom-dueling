bc307e8366e8231d092afe50d761634a1aede918
DLJam <alexjor72@gmail.com>
2022-09-17 15:28:52 -0400

    Update c62070235.lua

--- script/c62070235.lua
+++ script/c62070235.lua
@@ -43,7 +43,7 @@ function s.spcon(e,c)
 end
 
 function s.thfilter(c)
-	return c:IsSetCard(0x85a) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsCode(id) and c:IsAbleToHand()
+	return (c:IsSetCard(0x85a) or c:IsSetCard(0x1178)) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsCode(id) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
