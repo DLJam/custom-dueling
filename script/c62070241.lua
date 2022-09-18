aa5ed676573e46c53bb437aad3f4caa8537179b1
DLJam <alexjor72@gmail.com>
2022-09-18 17:43:26 -0400

    Snow Shard Update
    Reworked Snow Shard Storm

--- script/c62070241.lua
+++ script/c62070241.lua
@@ -1,6 +1,7 @@
 --Snow Shard Storm
 local s,id=GetID()
 function c62070241.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
 	--Search
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -9,14 +10,28 @@ function c62070241.initial_effect(c)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)  
-	--Prevent Activation
-	local e2=Effect.CreateEffect(c) 
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_CANNOT_TRIGGER)
-	e2:SetTarget(s.aclimit)
-	e2:SetValue(0) 
-	c:RegisterEffect(e2)  
+	--extra summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.sumcon)
+	e2:SetTarget(s.sumtg)
+	e2:SetOperation(s.sumop)
+	c:RegisterEffect(e2)
+   --material
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.condition)
+	e3:SetTarget(s.mattg)
+	e3:SetOperation(s.matop)
+	c:RegisterEffect(e3)
 end
 
 s.listed_series={0x85a}
@@ -35,6 +50,50 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.aclimit(e,c,tp)
-	return (c:GetAttack()>c:GetBaseAttack() or c:GetDefense()>c:GetBaseDefense() or c:GetAttack()<c:GetBaseAttack() or c:GetDefense()<c:GetBaseDefense()) and tc:IsActiveType(TYPE_MONSTER)
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsPlayerCanAdditionalSummon(tp)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanSummon(tp) end
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x8e))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.tgfilter(c)
+	return c:IsFaceup() and (c:IsSetCard(0x85a) or c:IsSetCard(0x84a)) and c:IsType(TYPE_XYZ)
+end
+function s.mfilter(c)
+	return c:IsSetCard(0x85a) and c:IsType(TYPE_MONSTER)
+end
+function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(s.mfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g1=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local g2=Duel.SelectTarget(tp,s.mfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g2,1,0,0)
+end
+function s.matop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=e:GetLabelObject()
+	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,tc,e)
+	if #g>0 then
+		Duel.Overlay(tc,g)
+	end
 end
