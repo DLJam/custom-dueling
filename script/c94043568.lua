7742fe2977cf0ca160aa02268a882bc00abc171b
DLJam <alexjor72@gmail.com>
2022-09-20 09:02:13 -0400

    Incant-Update 9//20/22
    "Incant-Mage Rayus"
    Script Finished
    Updated text to match effect
    Image Added
    
    "Incant-Staff" of Wonder
    Script finished
    Image Added

--- script/c94043568.lua
+++ script/c94043568.lua
@@ -27,8 +27,11 @@ function c94043568.initial_effect(c)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.spcon)
 	e3:SetCost(s.cost)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
@@ -71,7 +74,11 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-
+function s.spcon(e)
+    local c=e:GetHandler()
+    local eg=c:GetEquipGroup()
+    return #eg>0 and eg:IsExists(Card.IsSetCard,1,nil,0xa14)
+end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
@@ -81,17 +88,37 @@ function s.spfilter(c,e,tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ft=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),2)
+	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	if ft<=0 then return end
-	if ft>2 then ft=2 end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,ft,nil,e,tp)
-	if #g~=0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,ft,nil,e,tp)
+	if #g>0 then
+		local tc=g:GetFirst()
+		for tc in aux.Next(g) do
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+			local e3=Effect.CreateEffect(e:GetHandler())
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_UPDATE_LEVEL)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e3:SetValue(4)
+			tc:RegisterEffect(e3)
+		end
+		Duel.SpecialSummonComplete()
 	end
 end
--- script/c94043569.lua
+++ script/c94043569.lua
@@ -1,6 +1,7 @@
 --Incant-Staff of Wonder
 local s,id=GetID()
 function c94043569.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
 	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsSetCard,0xa14))
 	--increase ATK
 	local e2=Effect.CreateEffect(c)
@@ -18,7 +19,26 @@ function c94043569.initial_effect(c)
 	e3:SetCost(s.drcost)
 	e3:SetTarget(s.drtg)
 	e3:SetOperation(s.drop)
-	c:RegisterEffect(e3)	
+	c:RegisterEffect(e3)
+	--indestructable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e4:SetCountLimit(1)
+	e4:SetValue(s.valcon)
+	c:RegisterEffect(e4)	
+	--equip if sent to GY
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_EQUIP)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetCountLimit(1,{id,1})
+	e5:SetCondition(s.eqcon)
+	e5:SetTarget(s.eqtg)
+	e5:SetOperation(s.eqop)
+	c:RegisterEffect(e5)
 end
 
 s.listed_series={0xa14}
@@ -40,4 +60,33 @@ end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
+end
+
+function s.valcon(e,re,r,rp)
+	return (r&REASON_BATTLE)~=0
+end
+
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():CheckUniqueOnField(tp)
+end
+function s.eqfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xa14)
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.eqfilter(chkc) end
+	if chk==0 then return c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) and tc:IsFaceup()
+		and tc:IsSetCard(0xa14) and tc:IsControler(tp) and c:CheckUniqueOnField(tp) then
+		Duel.Equip(tp,c,tc)
+	end
 end
