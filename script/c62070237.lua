3b2e19ea7ccd4f73d6cf2d9216aa9cfefae54ed0
DLJam <alexjor72@gmail.com>
2022-12-07 09:39:01 -0500

    Create c62070237.lua

---
+++ script/c62070237.lua
@@ -0,0 +1,48 @@
+--Ranger of the Snow Shard, Arctic
+local s,id=GetID()
+function c62070237.initial_effect(c)
+	--If normal summoned, send 1 "Snow Shard" monster from deck to GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
+	c:RegisterEffect(e1)
+	--Can be treated as a level 4 or 5 for the Xyz summon of a WATER or "Number" monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_XYZ_LEVEL)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.xyzlv)
+	c:RegisterEffect(e2)
+end
+
+s.listed_series={0x85a,0x48}
+
+function s.tgfilter(c)
+	return c:IsSetCard(0x85a) and c:IsLevelAbove(5) and c:IsAbleToGrave()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
+
+function s.xyzlv(e,c,rc)
+	if rc:IsSetCard(0x85a) or rc:IsSetCard(0x48) then
+		return 4,5,e:GetHandler():GetLevel()
+	else
+		return e:GetHandler():GetLevel()
+	end
+end
