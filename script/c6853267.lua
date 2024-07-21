14894c883ee386c6a93297f91fbde37300fdd8a3
DLJam <alexjor72@gmail.com>
2024-07-21 07:46:16 -0400

    DL Updates
    Updated the effects for Cheviler, Renewal
    
    Cleaned up the effect for Decimator, Destruction
    
    Removed Borrelbarrage - Replaced with White Night
    
    Updated Burst Gunfire to Bursting Gunfire
    
    Updated to Roar of the Silver Dragon Lord to Silver and gold dragon lords
    
    Change Blrave to Felbrave
    
    Changed Buster steam to Blue Ice
    
    Changing Great Wing's Gift effect
    
    Added Quick-play Spell Culmination Of Dragon Lord's Fire
    
    Added Risvoltion Cresent and Otherworldly Dragon's Dark Might

--- script/c6853267.lua
+++ script/c6853267.lua
@@ -1,25 +1,25 @@
 --Armored Dragon Lord, Great Wing
 local s,id=GetID()
 function c6853267.initial_effect(c)
-    --Draw
-    local e1=Effect.CreateEffect(c)
+	--Draw
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-    e1:SetCondition(s.dcon)
-    e1:SetTarget(s.dtg)
-    e1:SetOperation(s.dop)
-    c:RegisterEffect(e1)    
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.dcon)
+	e1:SetTarget(s.dtg)
+	e1:SetOperation(s.dop)
+	c:RegisterEffect(e1)	
 	--Add 1 banish "DL"
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_OATH)
+	e2:SetCountLimit(1,{id,1})
 	e2:SetCost(s.thcost)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
@@ -37,22 +37,22 @@ s.listed_series={0x41A}
 s.listed_names={id}
 
 function s.dcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsSummonLocation(LOCATION_GRAVE)
+	return e:GetHandler():IsSummonLocation(LOCATION_GRAVE)
 end
 function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(2)
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
-    Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
 function s.dop(e,tp,eg,ep,ev,re,r,rp)
-    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-    if Duel.Draw(p,2,REASON_EFFECT)==2 then
-        Duel.ShuffleHand(tp)
-        Duel.BreakEffect()
-        Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
-    end
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	if Duel.Draw(p,2,REASON_EFFECT)==2 then
+		Duel.ShuffleHand(tp)
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
 end
 
 function s.cfilter(c)
@@ -94,41 +94,12 @@ end
 function s.efop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=c:GetReasonCard()
-	local e1=Effect.CreateEffect(rc)
-	e1:SetDescription(aux.Stringid(id,3))
-	e1:SetCategory(CATEGORY_TOHAND)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTarget(s.btarget)
-	e1:SetOperation(s.drop)
+	--Indestructible Effect Gift
+	e1:SetDescription(3000)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(1)
 	rc:RegisterEffect(e1,true)
-	if not rc:IsType(TYPE_EFFECT) then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_ADD_TYPE)
-		e2:SetValue(TYPE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		rc:RegisterEffect(e2,true)
-	end
-end
-
-
-function s.thfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x41A) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
-end
-function s.btarget(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,0,tp,1)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
 end
