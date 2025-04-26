429e5b2655e2f94d7359893bf4aa597aa5dba522
Tai Xiao Xuan <xiaoxuan.tai@digipen.edu>
2025-04-27 01:28:39 +0800

    Update c60681108.lua

--- script/c60681108.lua
+++ script/c60681108.lua
@@ -15,7 +15,7 @@ function c60681108.initial_effect(c)
 	
 end
 function s.chngtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(1,rp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,rp,0,LOCATION_MZONE,1,nil) end
 end
 function s.chngop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Group.CreateGroup()
@@ -24,9 +24,9 @@ function s.chngop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,1,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g2=Duel.SelectMatchingCard(1-tp,1,tp,0,LOCATION_MZONE,1,1,nil)
+	local g2=Duel.SelectMatchingCard(1-tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
 	g.AddCard(g,g2)
 	if #g>0 then
 		Duel.HintSelection(g)
