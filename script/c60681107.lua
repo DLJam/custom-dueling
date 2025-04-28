2364fb7a9a7053467c37e5ad129698bebfd384ce
DLJam <alexjor72@gmail.com>
2025-04-28 12:17:01 -0400

    Update c60681107.lua

--- script/c60681107.lua
+++ script/c60681107.lua
@@ -1,6 +1,5 @@
 --OtherShadow Drago-Emperor, Lost Cresent
 local s,id=GetID()
-
 function c60681107.initial_effect(c)
 	--Search or Mill
 	local e1=Effect.CreateEffect(c)
@@ -9,6 +8,7 @@ function c60681107.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)	
+	e1:SetCost(s.thcost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
@@ -20,12 +20,28 @@ function c60681107.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(s.xyzlv)
 	c:RegisterEffect(e2)
+	--Special Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 
 end
 
 s.listed_series={0x41a}
 s.listed_names={id}
 
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST|REASON_DISCARD)
+end
 function s.cfilter(c)
 	return c:IsMonster() and c:IsRace(RACE_DRAGON) and c:IsLevel(8) and c:IsAttribute(ATTRIBUTE_DARK) or c:IsSetCard(0x41a) and c:IsType(TYPE_SPELL+TYPE_TRAP) and (c:IsAbleToHand() or c:IsAbleToGrave())
 end
@@ -46,4 +62,34 @@ function s.xyzlv(e,c,rc)
 	else
 		return e:GetHandler():GetLevel()
 	end
+end
+
+function s.splimit(e,c)
+	return not c:IsRace(RACE_DRAGON) or c:IsRace(RACE_WYRM)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return (#g==0 or (#g>0 and g:FilterCount(aux.FaceupFilter(Card.IsRace,RACE_DRAGON) or aux.FaceupFilter(Card.IsRace,RACE_WYRM),nil)==#g))
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) then
+		--Place it on the bottom of the Deck if it leaves the field
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3301)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_DECKBOT)
+		e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+		c:RegisterEffect(e1)
+	end
+	Duel.SpecialSummonComplete()
 end
