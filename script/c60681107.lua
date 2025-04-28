974edb1a91b49e56b6b012b883005753ad787ec4
DLJam <alexjor72@gmail.com>
2025-04-28 12:01:03 -0400

    Update c60681107.lua

--- script/c60681107.lua
+++ script/c60681107.lua
@@ -1,4 +1,49 @@
 --OtherShadow Drago-Emperor, Lost Cresent
+local s,id=GetID()
+
 function c60681107.initial_effect(c)
-	
+	--Search or Mill
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)	
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Can be treated as Level 8 an Xyz
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_XYZ_LEVEL)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.xyzlv)
+	c:RegisterEffect(e2)
+
+end
+
+s.listed_series={0x41a}
+s.listed_names={id}
+
+function s.cfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_DRAGON) and c:IsLevel(8) and c:IsAttribute(ATTRIBUTE_DARK) or c:IsSetCard(0x41a) and c:IsType(TYPE_SPELL+TYPE_TRAP) and (c:IsAbleToHand() or c:IsAbleToGrave())
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	aux.ToHandOrElse(tc,tp)
+end
+
+function s.xyzlv(e,c,rc)
+	if rc:IsSetCard(0x41a) and rc:IsAttribute(ATTRIBUTE_DARK) then
+		return 8,e:GetHandler():GetLevel()
+	else
+		return e:GetHandler():GetLevel()
+	end
 end
