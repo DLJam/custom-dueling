75ce69345534d2d5c1d69277599d78e5d4b1bffd
DLJam <alexjor72@gmail.com>
2025-04-28 12:40:37 -0400

    Drago-Emperor Update 7
    Beckes you closer....

--- script/c6853348.lua
+++ script/c6853348.lua
@@ -1,4 +1,60 @@
 --Xenomorphic Dragon Lord, Risvoltion "Beckoner"
+local s,id=GetID()
 function c6853348.initial_effect(c)
-	
+	--Xyz summon
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,s.xyzfilter,8,2)
+
+	--Cannot be destroyed by effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))   
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCondition(s.setcon)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+end
+
+s.listed_series={0x41A}
+
+function s.xyzfilter(c,xyz,sumtype,tp)
+	return c:IsType(TYPE_XYZ,xyz,sumtype,tp) and 
+end
+
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) or e:GetHandler():IsSummonLocation(LOCATION_GRAVE)
+end
+function s.setfilter(c)
+	return c:IsTrap() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+end
+
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local tc=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.SSet(tp,tc)
+		--Can be activated this turn
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+		e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
 end
