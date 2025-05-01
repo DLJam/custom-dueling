03b96582d55cf45d689f1d90e8644a559f8d471b
DLJam <alexjor72@gmail.com>
2025-04-30 20:42:09 -0400

    Create c2143602.lua

---
+++ script/c2143602.lua
@@ -0,0 +1,25 @@
+--Three Musketeers - Bellestarr
+local s,id=GetID()
+function c2143602.initial_effect(c)
+	--Special Summon this card from your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thpop)
+	c:RegisterEffect(e1)	
+end
+
+function s.selfcostfilter(c,tp)
+	return c:IsSetCard(0x102c) and c:IsAbleToGraveAsCost()
+end
+function s.selfspcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.selfcostfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.selfcostfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
