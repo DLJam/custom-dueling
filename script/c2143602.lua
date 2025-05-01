a4ca637821c1a1681050c47c8325ddaab2e2ec6b
DLJam <alexjor72@gmail.com>
2025-04-30 20:47:47 -0400

    Rising Cards

--- script/c2143602.lua
+++ script/c2143602.lua
@@ -14,12 +14,29 @@ function c2143602.initial_effect(c)
 	c:RegisterEffect(e1)	
 end
 
+s.listed_series={0x102c}
+
 function s.selfcostfilter(c,tp)
 	return c:IsSetCard(0x102c) and c:IsAbleToGraveAsCost()
 end
-function s.selfspcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.selfcostfilter,tp,LOCATION_EXTRA,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.selfcostfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x102c) and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
